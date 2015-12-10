9b4a498a4089592d4dc678e3770974e37798c0f8
b-long <b-long@users.noreply.github.com>
2015-12-10 10:35:38 -0500

    WIP: Appropriate changes to settings.py
    Now that spb_history is in it's own package, we need to configure
    it differently.  For starters, the url module and the sqlite
    files are at the root of this package.

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         # todo - avoid hardcoding full path to database
-        'NAME': '/Users/dtenenba/dev/packagebuilder/spb_history/spb_history.sqlite',                      # Or path to database file if using sqlite3.
+        'NAME': 'spb_history.sqlite',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -102,7 +102,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'spb_history.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
