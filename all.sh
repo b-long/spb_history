84ed3f70f1e948f6caad4405081a41c553daec9e
Lori Shepherd <shepherl@gmail.com>
2016-10-04 12:38:13 -0400

    Change how track_build_completion is initialized

--- all.sh
+++ all.sh
@@ -6,5 +6,6 @@ killall python
 
 ./run-django.sh
 ./run-archiver.sh
+./run-track_build_completion.sh
 ./otherscripts.sh
 
--- otherscripts.sh
+++ otherscripts.sh
@@ -2,6 +2,5 @@
 
 . env/bin/activate
 
-nohup python track_build_completion.py > track_build_completion.log 2>&1 &
 nohup python pinger.py > pinger.log 2>&1 & 
 
