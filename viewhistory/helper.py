f7f31750c1be4c21a1b00b7578872ec2ceaa62c9
Hervé Pagès <hpages@fredhutch.org>
2019-11-18 10:27:16 -0800

    Remove stompy and MySQL-python modules from list of deps
    Also temporarily depend on the python3 branch of bioc-common-python (until
    it gets merged into the master branch).

--- viewhistory/helper.py
+++ viewhistory/helper.py
@@ -41,13 +41,13 @@ def filter_out_wrong_versions(builds, job):
     # FIXME get this info from a central source (config.yaml)
     if (r_ver == "3.6"):
         if bioc_version == "3.10":
-	    nodes = BUILD_NODES
+            nodes = BUILD_NODES
     if (r_ver == "3.6"):
         if bioc_version == "3.9":
-	    nodes = ["malbec2", "tokay2", "celaya2"]
+            nodes = ["malbec2", "tokay2", "celaya2"]
     if (r_ver == "3.5"):
         if bioc_version == "3.8":
-	    nodes = ["malbec1", "tokay1", "merida1"]
+            nodes = ["malbec1", "tokay1", "merida1"]
     if (r_ver == "3.5"):
         if bioc_version == "3.7":
             nodes = ["malbec2", "tokay2", "merida2"]
